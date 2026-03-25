31b7f2ee48dd0cfdd2da0226723d9a5f59d180d1
Vladyslav Bilokon <vlad2004bil@gmail.com>
2026-03-25 18:57:37 +0100

    Add vercel for backend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import gamesRouter from './routes/games.js'
 import authRouter from './routes/auth.js'
-
 import { initCache } from './middleware/cache.js'
 
 dotenv.config()
@@ -12,10 +11,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
 initCache()
 app.use('/api', gamesRouter)
 app.use('/api/auth', authRouter)
 
 const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.log(`Server on port ${PORT}`))
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`Server on port ${PORT}`))
+}
+
+export default app
